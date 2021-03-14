b3955704a2840705678edc7df0ace19f9881b136
Uberacy Tottoli <uberacy@trillion.com.br>
2021-03-14 10:34:02 +0100

    Add project files.

---
+++ Utils/ILocalStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace BlazorLocalStorageExample.Utils
+{
+    public interface ILocalStorage
+    {
+       Task SetAsync<T>(string key, T value);
+       Task<T> GetAsync<T>(string key);
+    }
+}
